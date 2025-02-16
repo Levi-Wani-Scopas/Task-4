742191f172c34c1efa0dea3f1f0a6bb0be034014
Levi-Wani-Scopas <leviscopas17@gmail.com>
2025-02-16 12:24:41 +0300

    File Model

--- app/src/main/java/com/example/taskfour/MainActivity.java
+++ app/src/main/java/com/example/taskfour/MainActivity.java
@@ -1,21 +1,102 @@
 package com.example.taskfour;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private FileAdapter fileAdapter;
+    private List<FileModel> fileList;
+    private DatabaseReference databaseRef;
+    Button btnprofile, btnupload;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        fileList = new ArrayList<>();
+        fileAdapter = new FileAdapter(this, fileList);
+        recyclerView.setAdapter(fileAdapter);
+
+        databaseRef = FirebaseDatabase.getInstance().getReference("Resources");
+        fetchFiles();
+
+
+        btnprofile = findViewById(R.id.btnProfile);
+        btnupload = findViewById(R.id.btnUpload);
+
+        btnprofile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+            }
+        });
+
+        btnupload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, UploadActivity.class));
+            }
+        });
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        String token = task.getResult();
+                        Log.d("FCM Token", token);
+                    }
+                });
+
+
+    }
+
+    private void fetchFiles() {
+        databaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                fileList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    FileModel file = dataSnapshot.getValue(FileModel.class);
+                    fileList.add(file);
+                }
+                fileAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(MainActivity.this, "Failed to load files", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
